51920b315d45ce97fc920880c5668c71c3ba62db
Markus Prepens <markus.prepens@gmail.com>
2019-12-01 23:02:27 +0100

    work with an array of repos

--- pack/update.sh
+++ pack/update.sh
@@ -1,35 +1,17 @@
-if [ -d Github/start/ctrlp ]; 
-then {
-  pushd Github/start/ctrlp
-  git pull
-  popd
-};
-else {
-  mkdir -p Github/start/ctrlp
-  git clone https://github.com/ctrlpvim/ctrlp.vim.git Github/start/ctrlp
-};
-fi
-
-if [ -d Github/start/easy-align ]; 
-then {
-  pushd Github/start/easy-align
-  git pull
-  popd
-};
-else {
-  mkdir -p Github/start/easy-align
-  git clone https://github.com/junegunn/vim-easy-align.git Github/start/easy-align
-};
-fi
-
-if [ -d Github/start/tagbar ]; 
-then {
-  pushd Github/start/tagbar
-  git pull
-  popd
-};
-else {
-  mkdir -p Github/start/tagbar
-  git clone https://github.com/majutsushi/tagbar.git  Github/start/tagbar
-};
-fi
+repos=(https://github.com/ctrlpvim/ctrlp.vim.git \
+           https://github.com/junegunn/vim-easy-align.git \
+           https://github.com/majutsushi/tagbar.git)
+for r in ${repos[*]} ; do {
+    s=${r##*/}
+    if [ -d Github/start/$s ];
+    then {
+        pushd Github/start/$s
+        git pull
+        popd
+    };
+    else {
+        mkdir -p Github/start/$s
+        git clone $r Github/start/$s
+    };
+    fi
+}; done
