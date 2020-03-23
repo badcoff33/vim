75f59bd3d94c4d8182f329f7f1b06db1cb58a4ad
badcoff33 <none@none>
2020-03-24 00:00:29 +0100

    removed useless comment

--- pack/update.py
+++ pack/update.py
@@ -1,10 +1,10 @@
 import os
 
+github_home_dir = "Github/start/"
 git_dict = { "easy-align": "https://github.com/junegunn/vim-easy-align.git",
         "vista": "https://github.com/liuchengxu/vista.vim.git",
         "LangClient": "https://github.com/autozimu/LanguageClient-neovim.git",
         "dirvish": "https://github.com/justinmk/vim-dirvish.git" }
-github_home_dir = "Github/start/"
 
 saved_pwd = os.getcwd()
 os.chdir( github_home_dir )
@@ -20,21 +20,5 @@ for subdir in git_dict:
         os.system( "git pull -q " + git_url )
         os.chdir( ".." )
 
-
 os.chdir( saved_pwd )
 
-#   echo "tail is :" %~nR
-# )
-# ::    s=${r##*/}
-# ::    if [ -d Github/start/$s ];
-# ::    then {
-# ::        pushd Github/start/$s
-# ::        git pull
-# ::        popd
-# ::    };
-# ::    else {
-# ::        mkdir -p Github/start/$s
-# ::        git clone $r Github/start/$s
-# ::    };
-# ::    fi
-# ::}; done
