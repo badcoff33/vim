0646833d2069e3592bb520307d119816e1422b9a
badcoff33 <none@none>
2023-08-09 13:22:58 +0200

    use fuzzyy

--- get_zpacks.bat
+++ get_zpacks.bat
@@ -4,39 +4,39 @@
 import os
 import requests
 import zipfile
+import glob
 
-unpack_to_start = [
-        "pack/unzipped/start/",
-        "https://github.com/mhinz/vim-signify/archive/refs/heads/master.zip",
-        "https://github.com/godlygeek/tabular/archive/refs/heads/master.zip",
-        "https://github.com/aklt/plantuml-syntax/archive/refs/heads/master.zip",
-        "https://github.com/preservim/tagbar/archive/refs/heads/master.zip"
-        ]
-
-unpack_to_opt = [
-        "pack/unzipped/opt/",
-        "https://github.com/junegunn/vim-easy-align/archive/refs/heads/master.zip",
-        "https://github.com/lilydjwg/colorizer/archive/refs/heads/master.zip"
-        ]
+unpack_tuple = (
+        ( "start", "https://github.com/mhinz/vim-signify/archive/refs/heads/master.zip" ),
+        ( "start", "https://github.com/Donaldttt/fuzzyy/archive/refs/heads/vim9.zip" ),
+        ( "opt",   "https://github.com/dhruvasagar/vim-table-mode/archive/refs/heads/master.zip" ),
+        ( "start", "https://github.com/aklt/plantuml-syntax/archive/refs/heads/master.zip" ),
+        ( "start", "https://github.com/preservim/tagbar/archive/refs/heads/master.zip" ),
+        ( "opt",   "https://github.com/junegunn/vim-easy-align/archive/refs/heads/master.zip" ),
+        ( "opt",   "https://github.com/lilydjwg/colorizer/archive/refs/heads/master.zip" ),
+        ( "skip",  "https://github.com/godlygeek/tabular/archive/refs/heads/master.zip" ),
+        )
 
 zip_fname = os.getenv("TEMP") + os.sep + "pack_download.zip"
 pop_back_dir = os.getcwd()
 
 def unpack_list(list):
-    if not os.path.isdir(list[0]):
-        os.makedirs(list[0])
-    print("for base dir {0}".format(list[0]))
-    for url in list[1:]:
-        print("get %s" % (url), end = "")
-        r = requests.get(url)
-        if os.path.isfile(zip_fname):
-            os.remove(zip_fname)
-        open(zip_fname, 'wb').write(r.content)
-        with zipfile.ZipFile(zip_fname, 'r') as zip_ref:
-            zip_ref.extractall(list[0])
-            print(", unzipped in " + zip_ref.namelist()[0])
+    for archive_desc in list:
+        url = archive_desc[1]
+        print("-- %s ... " % (url), end = "")
+        if archive_desc[0] == "skip":
+            print("skipping")
+        else:
+            target_dir = "pack/zpacks/" + archive_desc[0]
+            if not os.path.isdir(target_dir):
+                os.makedirs(target_dir)
+            print("downloading", end = "")
+            r = requests.get(url)
+            open(zip_fname, 'wb').write(r.content)
+            with zipfile.ZipFile(zip_fname, 'r') as zip_ref:
+                zip_ref.extractall(target_dir)
+            print(",unzipped")
 
-unpack_list(unpack_to_start)
-unpack_list(unpack_to_opt)
+unpack_list(unpack_tuple)
 
 # vim:ft=python:
