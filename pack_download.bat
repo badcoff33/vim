f52517d399c620053d0ea8deb2df42d45e908649
badcoff33 <none@none>
2024-02-27 09:51:56 +0100

    rename cryptic unzip dirs to readable versions

--- pack_download.bat
+++ pack_download.bat
@@ -1,21 +1,25 @@
 @setlocal enableextensions & python -x %~f0 %* & goto :EOF
 ### Python code:
 
+# from os import listdir
+# from os.path import isfile, join
+# onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
 import os
 import requests
 import zipfile
 import glob
 
 unpack_tuple = (
-        ("start", "https://github.com/mhinz/vim-signify/archive/refs/heads/master.zip"),
-        ("opt", "https://github.com/junegunn/vim-easy-align/archive/refs/heads/master.zip"),
-        ("start", "https://github.com/preservim/tagbar/archive/refs/heads/master.zip"),
-        ("start", "https://github.com/justinmk/vim-dirvish/archive/refs/heads/master.zip"),
-        ("opt", "https://github.com/dhruvasagar/vim-table-mode/archive/refs/heads/master.zip"),
-        ("start", "https://github.com/godlygeek/tabular/archive/refs/heads/master.zip"),
-        ("skip", "https://github.com/Donaldttt/fuzzyy/archive/refs/heads/vim9.zip"),
-        ("skip", "https://github.com/aklt/plantuml-syntax/archive/refs/heads/master.zip"),
-        ("skip", "https://github.com/preservim/vim-wordy/archive/refs/heads/master.zip"))
+        ("start", "https://github.com/mhinz/vim-signify/archive/refs/heads/master.zip", "signify"),
+        ("opt", "https://github.com/junegunn/vim-easy-align/archive/refs/heads/master.zip", "easy-align"),
+        ("start", "https://github.com/preservim/tagbar/archive/refs/heads/master.zip", "tagbar"),
+        ("start", "https://github.com/justinmk/vim-dirvish/archive/refs/heads/master.zip", "dirvish"),
+        ("opt", "https://github.com/dhruvasagar/vim-table-mode/archive/refs/heads/master.zip", "table-mode"),
+        ("start", "https://github.com/godlygeek/tabular/archive/refs/heads/master.zip", "tabular"),
+        ("skip", "https://github.com/Donaldttt/fuzzyy/archive/refs/heads/vim9.zip", ""),
+        ("skip", "https://github.com/aklt/plantuml-syntax/archive/refs/heads/master.zip", ""),
+        ("skip", "https://github.com/preservim/vim-wordy/archive/refs/heads/master.zip", ""))
 
 zip_fname = os.getenv("TEMP") + os.sep + "pack_download.zip"
 pop_back_dir = os.getcwd()
@@ -23,6 +27,7 @@ pop_back_dir = os.getcwd()
 def unpack_list(list):
     for archive_desc in list:
         url = archive_desc[1]
+        rename_to = archive_desc[2]
         print("-- %s ... " % (url), end = "")
         if archive_desc[0] == "skip":
             print("skipping")
@@ -33,9 +38,15 @@ def unpack_list(list):
             print("downloading", end = "")
             r = requests.get(url)
             open(zip_fname, 'wb').write(r.content)
-            with zipfile.ZipFile(zip_fname, 'r') as zip_ref:
-                zip_ref.extractall(target_dir)
-            print(",unzipped")
+            z = zipfile.ZipFile(zip_fname, 'r')
+            z_root = z.namelist()[0]
+            z.extractall(target_dir)
+            if rename_to != "":
+                print(",unzipped,renamed")
+                os.rename(target_dir + os.sep + z_root[:-1], target_dir + os.sep + rename_to)
+            else:
+                print(",unzipped")
+
 
 unpack_list(unpack_tuple)
 
