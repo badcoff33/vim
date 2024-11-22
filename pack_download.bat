358d61f97493256bf0f2249756b1f592ba122163
badcoff33 <none@none>
2024-11-22 08:52:08 +0100

    replaced Tagbar against Vista

--- pack_download.bat
+++ pack_download.bat
@@ -13,6 +13,7 @@ import glob
 # Move all packages to 'opt' dir. Load the packages from user.vim file with ':packadd ...'
 unpack_tuple = (
         ("start", "https://github.com/godlygeek/tabular/archive/refs/heads/master.zip", "tabular"),
+        ("start", "https://github.com/liuchengxu/vista.vim/archive/refs/heads/master.zip", "vista"),
         ("opt", "https://github.com/mhinz/vim-signify/archive/refs/heads/master.zip", "signify"),
         ("opt", "https://github.com/preservim/tagbar/archive/refs/heads/master.zip", "tagbar"),
         ("skip", "https://github.com/junegunn/vim-easy-align/archive/refs/heads/master.zip", "easy-align"),
@@ -22,32 +23,27 @@ unpack_tuple = (
         ("skip", "https://github.com/aklt/plantuml-syntax/archive/refs/heads/master.zip", ""),
         ("skip", "https://github.com/preservim/vim-wordy/archive/refs/heads/master.zip", ""))
 
-zip_fname = os.getenv("TEMP") + os.sep + "pack_download.zip"
-pop_back_dir = os.getcwd()
-
 def unpack_list(list):
+    zip_fname = os.getenv("TEMP") + os.sep + "pack_download.zip"
     for archive_desc in list:
+        pack_dir = archive_desc[0]
         url = archive_desc[1]
         rename_to = archive_desc[2]
-        print("-- %s ... " % (url), end = "")
-        if archive_desc[0] == "skip":
-            print("skipping")
+        if pack_dir == "skip":
+            print("[SKIPPING] %s" % (url), end = "\n")
         else:
-            target_dir = "pack/downloads/" + archive_desc[0]
+            target_dir = "pack/downloads/" + pack_dir
             if not os.path.isdir(target_dir):
                 os.makedirs(target_dir)
-            print("downloading", end = "")
+            print("[DOWNLOAD] %s" % (url), end = "")
             r = requests.get(url)
             open(zip_fname, 'wb').write(r.content)
             z = zipfile.ZipFile(zip_fname, 'r')
             z_root = z.namelist()[0]
             z.extractall(target_dir)
             if rename_to != "":
-                print(",unzipped,renamed")
+                print(" unpacked in %s" % (rename_to), end = "\n")
                 os.rename(target_dir + os.sep + z_root[:-1], target_dir + os.sep + rename_to)
-            else:
-                print(",unzipped")
-
 
 unpack_list(unpack_tuple)
 
